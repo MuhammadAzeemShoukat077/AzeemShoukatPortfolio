8214feb1c7b4101e2be6ffbddee3d276f6f9fd0e
MuhammadAzeemShoukat077 <azeemshoukat000@gmail.com>
2024-02-13 15:20:14 +0500

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,14 +10,14 @@ export const Hero = () => {
         <h1 className={styles.title}>Hi, I'm Azeem</h1>
         <p className={styles.description}>
           I'm a full-stack developer having experience in frontend and backend development using technologies like React and
-          NodeJS. Reach out if you'd like to learn more!
+          NodeJS. Reach out if you'd like to know more!
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:azeemshoukat000@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/image1.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
