df06221cd23733b54db1589ad1bfd801639a61d6
MuhammadAzeemShoukat077 <azeemshoukat000@gmail.com>
2024-02-13 14:09:42 +0500

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+        <p>Mobile:(+92)3038454507</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:azeemshoukat000@gmail.com">mail</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/azeem-shoukat-2a0a55230">
+            linkedin
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/MuhammadAzeemShoukat077">github</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      {isMobile ? (
+        <div className={styles.menu}>
+          <img
+            className={styles.menuBtn}
+            src={
+              menuOpen
+                ? getImageUrl("nav/closeIcon.png")
+                : getImageUrl("nav/menuIcon.png")
+            }
+            alt="menu-button"
+            onClick={() => setMenuOpen(!menuOpen)}
+          />
+          <ul
+            className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}
+          >
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <ul className={styles.menuItems}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
